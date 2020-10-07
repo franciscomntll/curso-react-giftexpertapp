df2dbeec541dafde55260c03efab42da40b8f814
Francisco_Montilla <ciscomontilla@gmail.com>
2020-10-07 15:19:45 -0400

    docs

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const GifGridItem = ({id, title, url}) => {
     
     return (
-        <div className="card animate__animated animate__bounceInDown animate__delay-10s ">
+        <div className="card animate__animated animate__fadeIn animate__delay-10s ">
             <img src={url} alt={title}></img>
             <p>{title}</p>
         </div>
