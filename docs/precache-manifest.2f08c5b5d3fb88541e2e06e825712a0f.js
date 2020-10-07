b475d4dced93f979411b50ddb00a60b8e3034e2d
franciscomntll <71562685+franciscomntll@users.noreply.github.com>
2020-10-07 15:21:57 -0400

    Add files via upload

---
+++ docs/precache-manifest.2f08c5b5d3fb88541e2e06e825712a0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83ff3c2e4fce97aa16efc7c995c3d0ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db2a6af3dab8c8055c5f",
+    "url": "/static/css/main.0289b1bc.chunk.css"
+  },
+  {
+    "revision": "3bd7f3839b4867d08700",
+    "url": "/static/js/2.372dfa45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.372dfa45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db2a6af3dab8c8055c5f",
+    "url": "/static/js/main.5b48e2bd.chunk.js"
+  },
+  {
+    "revision": "4e3a1f73b6e5de40b557",
+    "url": "/static/js/runtime-main.5c054f69.js"
+  }
+]);
